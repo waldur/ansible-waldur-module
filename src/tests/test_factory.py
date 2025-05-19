5561e756df4ffd5bfbbe032da9d7d27e320720d3
Mark Borissov <markboriut@gmail.com>
2025-05-19 13:53:40 +0000

    [WAL-8719] Add base factory for generating values for new SDK tests

---
+++ src/tests/test_factory.py
@@ -0,0 +1,23 @@
+import pytest
+from .utils.factory import generate_example_instance, serialize_attrs_instance
+import waldur_api_client.models as models
+import json
+
+
+@pytest.mark.parametrize(
+    "model_class",
+    [
+        models.Project,
+        models.Offering,
+    ],
+)
+def test_multiple_models_serialization_cycle(model_class):
+    """Test serialization-deserialization cycle for multiple model types."""
+    original_instance = generate_example_instance(model_class)
+
+    instance_dict = serialize_attrs_instance(original_instance)
+
+    json_str = json.dumps(instance_dict)
+    instance_dict = json.loads(json_str)
+    reconstructed_instance = model_class.from_dict(instance_dict)
+    assert isinstance(reconstructed_instance, model_class)
